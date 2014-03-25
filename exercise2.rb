416507f741e21789c086fefd9eae234ae34aaf1c
Yong <ysk77@hotmail.com>
2014-03-25 14:51:14 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+puts 55 * 0.15
+
+puts "abc" + 123.to_s
+
+answer = 45628 * 7839
+
+puts "The result of 45628 multiplied by 7839 is #{answer}"
+
+puts (1 == 1 and 1 < 0) || (1 != 1 and 1 == 1 ) || !(0 > 1 and 1 > 2)  #true
