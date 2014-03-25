a62ebc3492ddb331787a2f1dc0e84c067719f942
Yong <ysk77@hotmail.com>
2014-03-25 15:16:21 -0400

    exercise3 done

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,14 @@
+
 puts "What's your name?"
+
+name = gets.chomp
+name.capitalize!
+
+puts "Hi #{name}!"
+
+puts "What's your age?"
+
+age = gets.chomp.to_i
+year = 2014 - age
+
+puts "You were born on year #{year}"
